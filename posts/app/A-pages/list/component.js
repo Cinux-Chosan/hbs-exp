20c04ddc3dbaf154f884aef99f97bc575ca0e0b8
Chosan <179817004@qq.com>
2016-11-04 18:36:32 +0800

    x

--- posts/app/A-pages/list/component.js
+++ posts/app/A-pages/list/component.js
@@ -7,6 +7,10 @@ export default Ember.Component.extend({
   myName(name) {
     return name;
   },
+  hours: 1,
+
+  longMessage: 'sdsaklfjalsdkfajslkdfjl了看风景啊离开司法局拉萨减肥卢卡斯觉得非拉萨减肥拉开司机法律；阿三觉得非拉开岁了；发送减肥狂拉萨觉得非卢卡斯就罚款拉萨减肥了',
+
   didInsertElement() {
     $.ajax({
       url: '/data',
@@ -16,5 +20,6 @@ export default Ember.Component.extend({
         this.set('age', r.name);
       }, 3000);
     });
+    setInterval(() => { this.set('hours', this.get('hours') + 1); }, 1000);
   }
 });
--- posts/app/transitions.js
+++ posts/app/transitions.js
@@ -12,4 +12,22 @@ export default function(){
    this.use('crossFade'),
    this.reverse('crossFade')
  );
+
+   this.transition(
+    this.childOf('#liquid-bind-demo'),
+    this.use('crossFade')
+  );
+
+  this.transition(
+  this.hasClass('vehicles'),
+
+  // this makes our rule apply when the liquid-if transitions to the
+  // true state.
+  this.toValue(true),
+  this.use('crossFade', {duration:200}),
+
+  // which means we can also apply a reverse rule for transitions to
+  // the false state.
+  this.reverse('toLeft', {duration:200})
+  );
 }
